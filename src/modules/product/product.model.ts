0a3c80fb442489d1dc15bfbfe44e34c4da3c22aa
Ashfakul Islam <r4pido3@gmail.com>
2024-05-20 19:31:20 +0600

    product model created

--- src/modules/product/product.model.ts
+++ src/modules/product/product.model.ts
@@ -1,9 +1,34 @@
-import { Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 import { Product } from "./product.interface";
 
 const productSchema = new Schema<Product>({
-    name:{type: String, required:[true, "Please provide a product name"]},
-    description:{type: String, required:[true, "Please provide a short description"]},
-    price:{type : Number, required:[true, ]}
-})
+  name: { type: String, required: [true, "Please provide a product name"] },
+  description: {
+    type: String,
+    required: [true, "Please provide a short description"],
+  },
+  price: { type: Number, required: [true, "Please provide the price"] },
+  category: { type: String, required: [true, "Please provide a category"] },
+  tags: { type: [String], required: [true, "Please provide product tags"] },
+  variants: {
+    type: [
+      {
+        type: { type: String, required: true },
+        value: { type: String, required: true },
+      },
+    ],
+    required: [true, "Please provide atleast one variant"],
+  },
+  inventory: {
+    quantity: {
+      type: Number,
+      required: [true, "Please provide product inventory quantity"],
+    },
+    inStock: {
+      type: Boolean,
+      default: true,
+    },
+  },
+});
 
+export const ProductModel = model<Product>("product", productSchema);
