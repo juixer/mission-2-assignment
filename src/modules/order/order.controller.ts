70d3d5983e974cefbe083fb507205ae795c15d99
Ashfakul Islam <r4pido3@gmail.com>
2024-05-21 03:38:41 +0600

    after creating an order quantity will reduce and if it hits 0 inStock value changes to false

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -2,10 +2,22 @@
 
 import { Request, Response } from "express";
 import { orderService } from "./order.service";
+import { ProductModel } from "../product/product.model";
 
 const createOrder = async (req: Request, res: Response) => {
   try {
     const order = req.body;
+    const productId = order.productId;
+
+    const { inventory }: any = await ProductModel.findById(productId);
+
+    if (inventory.inStock === false) {
+      return res.status(400).json({
+        success: false,
+        message: "Insufficient quantity available in inventory",
+      });
+    }
+
     const result = await orderService.createOrderIntoDB(order);
     res.status(200).json({
       screen: true,
--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,13 +1,32 @@
 // create order into db
 
+import { ProductModel } from "../product/product.model";
 import { Order } from "./order.interface";
 import { OrderModel } from "./order.model";
 
 const createOrderIntoDB = async (order: Order) => {
+  const id = order.productId;
+  const orderQuantity = order.quantity;
+
+  const { price }: any = await ProductModel.findById(id);
+
+  const totalPrice = price * orderQuantity;
+  order.price = totalPrice;
+
+  const updateProduct = await ProductModel.findByIdAndUpdate(id, {
+    $inc: { "inventory.quantity": -orderQuantity },
+  });
+
+  if (updateProduct?.inventory.quantity === 1) {
+    await ProductModel.findByIdAndUpdate(id, {
+      $set: { "inventory.inStock": false },
+    });
+  }
+
   const result = await OrderModel.create(order);
   return result;
 };
 
 export const orderService = {
-    createOrderIntoDB,
+  createOrderIntoDB,
 };
