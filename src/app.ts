43dfc6c7d5d20776994bde1b23e9ec4f5c290472
Ashfakul Islam <r4pido3@gmail.com>
2024-05-20 18:58:07 +0600

    eslint config setup done

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
-import express from 'express'
-const app = express()
-const port = 3000
+import express from "express";
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 export default app;
